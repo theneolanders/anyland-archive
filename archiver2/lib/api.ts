46a86b0e804074143ffd6332560f1cb0813ddf6e
Sisol <85529362+Eselvire@users.noreply.github.com>
2023-12-30 16:45:26 +0100

    Add proxy server

--- archiver2/lib/api.ts
+++ archiver2/lib/api.ts
@@ -91,3 +91,90 @@ export const mkApiReqs = async (sendNetRequest: (req: NetRequest) => void) => {
 
     return { get, post, bumpToken }
 }
+
+
+// Dirty copy-paste where get and post expect to be given a cookie. This is used for the proxy-server only
+export const mkApiReqs_cookie = async (sendNetRequest: (req: NetRequest) => void) => {
+    
+
+    // These functions abstract the API calls, headers, and send the req/res to NSQ for future reference or in case I mess something up in the archival process.
+
+    const get = async (reason: string, path: string, cookie: string | null) => {
+        const now = new Date();
+        const url = ANYLAND_ROOT + path;
+        const headers = {
+            'User-Agent': 'UnityPlayer/2018.1.0f2 (UnityWebRequest/1.0, libcurl/7.51.0-DEV)',
+            'Accept': '*/*',
+            'Accept-Encoding': 'identity',
+            'X-Unity-Version': '2018.1.0f2',
+        }
+
+        if (cookie) {
+            // @ts-ignore
+            headers['Cookie'] = cookie;
+        }
+
+        const res = await fetch(url, { headers })
+
+        const bodyTxt = await res.clone().text();
+        sendNetRequest({
+            ts: now.valueOf(),
+            ts_iso: now.toISOString(),
+            url,
+            reason,
+            method: "GET",
+            reqHeaders: headers,
+            reqbody: null,
+            resCode: res.status,
+            resHeaders: headersToObject(res.headers),
+            resText: bodyTxt,
+        })
+
+
+        return res;
+    }
+
+
+    const post = async (reason: string, path: string, body: string, cookie: string | null) => {
+        const now = new Date();
+        const url = ANYLAND_ROOT + path;
+        const headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'UnityPlayer/2018.1.0f2 (UnityWebRequest/1.0, libcurl/7.51.0-DEV)',
+            'Accept': '*/*',
+            'Accept-Encoding': 'identity',
+            'X-Unity-Version': '2018.1.0f2',
+        }
+
+        if (cookie) {
+            // @ts-ignore
+            headers['Cookie'] = cookie;
+        }
+
+        const res = await fetch(url, {
+            method: "POST",
+            headers: headers,
+            body,
+        })
+
+        const bodyTxt = await res.clone().text();
+        sendNetRequest({
+            ts: now.valueOf(),
+            ts_iso: now.toISOString(),
+            url,
+            reason,
+            method: "POST",
+            reqHeaders: headers,
+            reqbody: body,
+            resCode: res.status,
+            resHeaders: headersToObject(res.headers),
+            resText: bodyTxt,
+        })
+
+
+        return res;
+    }
+
+
+    return { get, post }
+}
