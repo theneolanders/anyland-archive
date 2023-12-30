2fb19e4a9936331aeaee6e2342364e43a2c97f08
Sisol <85529362+Eselvire@users.noreply.github.com>
2023-12-30 05:09:51 +0100

    Handle deleted/hidden(?) players

--- archiver2/archiver2.ts
+++ archiver2/archiver2.ts
@@ -290,21 +290,32 @@ const walkProps = (obj: unknown) => {
 //////////////////////////////
 //////////////////////////////
 
-const PersonInfoSchema = z.object({
-  id: z.string(),
-  screenName: z.string(),
-  age: z.number(),
-  statusText: z.string(),
-  isFindable: z.boolean(),
-  isBanned: z.boolean(),
-  lastActivityOn: z.string().datetime(),
-  isFriend: z.boolean(),
-  isEditorHere: z.boolean(),
-  isListEditorHere: z.boolean(),
-  isOwnerHere: z.boolean(),
-  isAreaLocked: z.boolean(),
-  isOnline: z.boolean()
-}).strict()
+const PersonInfoSchema = z.union([
+  z.object({
+    id: z.string(),
+    screenName: z.string(),
+    age: z.number(),
+    statusText: z.string(),
+    isFindable: z.boolean(),
+    isBanned: z.boolean(),
+    lastActivityOn: z.string().datetime(),
+    isFriend: z.boolean(),
+    isEditorHere: z.boolean(),
+    isListEditorHere: z.boolean(),
+    isOwnerHere: z.boolean(),
+    isAreaLocked: z.boolean(),
+    isOnline: z.boolean()
+  }).strict(),
+  // either deleted/nonexistent players, or some kind of unfindable setting?
+  z.object({
+    isFriend: z.boolean(),
+    isEditorHere: z.boolean(),
+    isListEditorHere: z.boolean(),
+    isOwnerHere: z.boolean(),
+    isAreaLocked: z.boolean(),
+    isOnline: z.boolean()
+  }).strict(),
+])
 
 
 const downloadPersonInfo = mkQuery(
