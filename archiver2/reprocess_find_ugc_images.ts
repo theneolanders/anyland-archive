fb946747f254ff08b3d9d0fc9c42903d0a925891
Sisol <85529362+Eselvire@users.noreply.github.com>
2024-01-03 13:37:37 +0100

    remove unecessary env vars from find_ugc_images

--- archiver2/reprocess_find_ugc_images.ts
+++ archiver2/reprocess_find_ugc_images.ts
@@ -1,16 +1,6 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
-import { findNestedIdsInThing } from './lib/utils';
-import { mkWriter } from './lib/nsq'
 
-
-if (!process.env.ANYLAND_COOKIE) throw "no cookie in env"
-if (!process.env.NSQD_HOST) throw "no cookie in env"
-
-const NSQD_HOST = process.env.NSQD_HOST;
-const NSQD_PORT = 4150;
-
-const { enqueueThing } = await mkWriter(NSQD_HOST, NSQD_PORT);
 
 const IMAGES_DIR = '../archiver/images/';
 
@@ -61,7 +51,7 @@ const files = await fs.readdir(DIR);
 
 for (let i = 0; i <= files.length; i++) {
   const filename = files[i];
-  console.log(i, "/", files.length, filename);
+  //console.log(i, "/", files.length, filename);
 
   try {
     const file = Bun.file(path.join(DIR, filename));
